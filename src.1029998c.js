768e9d59fc94d9e8652c8c2ea38596ec5653d8ca
maria-moroz <91492730+maria-moroz@users.noreply.github.com>
2022-05-04 21:26:45 +0000

    Deploying to gh-pages from @ maria-moroz/goit-filmoteka@8c4866133b94f0671ba6c87e9343be9f88562ee9 🚀

--- src.1029998c.js
+++ src.1029998c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-filmoteka/src.076183c1.js.map
+//# sourceMappingURL=/goit-filmoteka/src.1029998c.js.map

88cd977411a2e658ecef818c4c73c4bab2f045dd
Ruslan Moiseyenko <50419466+ruslan-moiseyenko@users.noreply.github.com>
2022-05-07 09:35:16 +0300

    finished all data fetching. still need to add pagination

--- src/js/renderFilmCard.js
+++ src/js/renderFilmCard.js
@@ -1,12 +1,18 @@
-export default function renderFilmCard({
-  root,
-  searchResults,
-  base_url,
-  poster_size,
-  backdrop_sizes,
-}) {
+export default function renderFilmCard({ root, searchResults, base_url, backdrop_sizes, genres }) {
   const { results } = searchResults;
   results.forEach(({ id, title, poster_path, vote_average, release_date, genre_ids }) => {
+    let genre1 = '';
+    let genre2 = '';
+    for (let item in genres) {
+      if (genre_ids[0] === genres[item].id) {
+        genre1 = genres[item].name;
+      }
+    }
+    for (let item in genres) {
+      if (genre_ids[1] === genres[item].id) {
+        genre2 = genres[item].name;
+      }
+    }
     root.insertAdjacentHTML(
       'beforeend',
       `
@@ -18,7 +24,7 @@ export default function renderFilmCard({
       />
       <h2 class="card-preview-info__name">${title}</h2>
       <div class="card-preview-info">
-        <p class="card-preview-info__data">Genre | ${parseInt(release_date)}</p>
+        <p class="card-preview-info__data">${genre1}, ${genre2} | ${parseInt(release_date)}</p>
         <span class="card-preview-info__rating">${vote_average}</span>
       </div>
     </div>`,

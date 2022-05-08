1def29e5a3b129f6e5010e63c09140b1802b2c89
maria-moroz <m0954008202@gmail.com>
2022-05-08 13:53:59 +0300

    Update theme-switcher.js

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -3,7 +3,7 @@ const checkbox = document.getElementById('checkbox');
 const body = document.querySelector('body');
 const footer = document.querySelector('footer');
 const footerText = document.querySelector('.footer_text');
-// const filmInfo = document.querySelector('.film-info');
+const filmInfo = document.querySelector('.film-info__overlay');
 
 checkbox.addEventListener('change', onDarkTheme);
 
@@ -11,4 +11,5 @@ function onDarkTheme() {
   body.classList.toggle('dark');
   footer.classList.toggle('dark');
   footerText.classList.toggle('dark');
+  filmInfo.classList.toggle('dark-modal');
 }

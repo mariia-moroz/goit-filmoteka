e106f40a4c62d26ffa8a76153e8872bff02c25a8
maria-moroz <m0954008202@gmail.com>
2022-05-11 17:30:53 +0300

    Update renderFilmCard.js

--- src/js/renderFilmCard.js
+++ src/js/renderFilmCard.js
@@ -1,12 +1,18 @@
 export default function renderFilmCard({ root, base_url, poster_size, movie, genresList, page }) {
   const { id, title, poster_path, vote_average, release_date } = movie;
   let src = '';
+  let date = '';
   if (poster_path) {
     src = `src="${base_url}${poster_size}${poster_path}" alt="${title}"`
   } else {
     src = '';
   }
 
+  if (release_date) {
+    date = parseInt(release_date);
+  } else {
+    date = '';
+  }
   root.insertAdjacentHTML(
     'beforeend',
     `
@@ -17,7 +23,7 @@ export default function renderFilmCard({ root, base_url, poster_size, movie, gen
       />
       <h2 class="card-preview-info__name">${title}</h2>
       <div class="card-preview-info">
-        <p class="card-preview-info__data">${genresList.slice(0, 2).join(', ')} | ${parseInt(release_date)}</p>
+        <p class="card-preview-info__data">${genresList.slice(0, 2).join(', ')} | ${date}</p>
         <span class="card-preview-info__rating">${vote_average}</span>
       </div>
     </div>`,

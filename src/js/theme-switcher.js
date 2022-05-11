f83b48760913f5842ad37de3b1edfcc51a34f9dd
Sergio Pieshko <sergio.pieshko@icloud.com>
2022-05-11 22:46:54 +0300

    fixes
    some fixes

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -4,6 +4,9 @@ const body = document.querySelector('body');
 const footer = document.querySelector('footer');
 const footerText = document.querySelector('.footer_text');
 const filmInfo = document.querySelector('.film-info__overlay');
+const switcher = document.querySelector('.switcher');
+const navLib = document.getElementById('library');
+const home = document.getElementById('home');
 
 checkbox.addEventListener('change', onDarkTheme);
 
@@ -13,3 +16,15 @@ function onDarkTheme() {
   footerText.classList.toggle('dark');
   filmInfo.classList.toggle('dark-modal');
 }
+
+navLib.addEventListener('click', () => {
+  if (navLib) {
+    switcher.classList.add('visually-hidden');
+  }
+});
+
+home.addEventListener('click', () => {
+  if (home) {
+    switcher.classList.remove('visually-hidden');
+  }
+});

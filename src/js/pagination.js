bb48770566bd57bffbc78a50f7a5e998531aea88
maria-moroz <m0954008202@gmail.com>
2022-05-11 15:00:07 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,6 @@
 import Pagination from 'tui-pagination';
 import renderPopFilms from './showMovieGallery';
+import { renderNameFilms } from './fetch-by-name';
 import { scrollToTop } from './scroll-to-top';
 
 const options = {
@@ -11,37 +12,42 @@ const options = {
   centerAlign: true,
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
 };
+
+const paginationOptions = {
+  key: '',
+}
 
 const pagination = new Pagination('pagination', options);
 
-export { pagination }
+export { pagination, paginationOptions }
 
 // внесення номеру сторінки в local Storage
 
 pagination.on('afterMove', onPaginationClick);
 
-function onPaginationClick(event) {
+export function onPaginationClick(event) {
   const currentPage = event.page;
   localStorage.setItem('pagination-page', JSON.stringify(currentPage));
-  renderPopFilms();
+  const value = paginationOptions.key;
+  console.log(value);
+  switch (value) {
+    case "popular":
+      renderPopFilms();
+      break;
+    case "search":
+      renderNameFilms();
+      break;
+  }
   scrollToTop();
+}
+
+export function showPagination() {
+  document.querySelector('.pagination_container').classList.remove('visually-hidden');
+}
+
+export function hidePagination() {
+  document.querySelector('.pagination_container').classList.add('visually-hidden');
 }
 
 // забираємо з local Storage номер сторінки

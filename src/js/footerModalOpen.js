5f605182e2348ba9c9b60829e391443d895caac2
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-05-11 20:16:53 +0300

    finish

--- src/js/footerModalOpen.js
+++ src/js/footerModalOpen.js
@@ -4,17 +4,27 @@ const refs = {
   modal: document.querySelector('[data-modal]'),
   bodyScroll: document.querySelector('body'),
   overlay: document.querySelector('.overlay'),
+  list: document.querySelector('.team'),
 };
 
 refs.openModalBtn.addEventListener('click', openModal);
+
+const array = [...refs.list.children];
 
 function openModal() {
   refs.modal.classList.toggle('footer__backdrop--hidden');
   refs.bodyScroll.classList.toggle('is-open');
   document.addEventListener('keydown', eventKeydown);
-  setTimeout(hideOverlay, 1500);
+
+  setTimeout(hideOverlay, 1000);
   setTimeout(showBackground, 3000);
   refs.closeModalBtn.addEventListener('click', closeModal);
+  array.reduce((acc, item) => {
+    acc += 250;
+    setTimeout(() => (item.style.opacity = '1'), acc + 500);
+    setTimeout(() => item.classList.remove('big'), (acc += 1000));
+    return acc;
+  }, 1000);
 }
 
 function closeModal() {
@@ -25,6 +35,10 @@ function closeModal() {
   refs.modal.classList.toggle('modal-bg');
   refs.closeModalBtn.removeEventListener('click', closeModal);
   document.removeEventListener('keydown', eventKeydown);
+  array.map(item => {
+    item.classList.add('big');
+    item.style.opacity = '0';
+  });
 }
 
 function hideOverlay() {

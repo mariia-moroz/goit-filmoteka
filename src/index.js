7bef8253b01f07691da444554c3ffb7da9352d9c
maria-moroz <m0954008202@gmail.com>
2022-05-13 13:05:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import showMovieGallery from './js/showMovieGallery';
 import './js/dynamicHeaderMarkup';
 import './js/fetch-by-name';
 import './js/filmInfoModalCreate';
-import './js/footerModalOpen';
+//import './js/footerModalOpen';
 import './js/add-to-watched';
 import './js/pagination';
 import '@fortawesome/fontawesome-free/js/all.js';
